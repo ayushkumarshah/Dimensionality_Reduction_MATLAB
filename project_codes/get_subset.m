0c110308b2db8cf41888ad24861f5754218d3842
Ayush Kumar Shah <ayush.kumar.shah@gmail.com>
2020-11-29 00:16:51 +0545

    feat: Add ability to choose num of faces

--- project_codes/get_subset.m
+++ project_codes/get_subset.m
@@ -1,12 +1,15 @@
-function images_sub = get_subset(images, num_subjects)
+function indices = get_subset(num_subjects, num_faces)
 % Get subset of images from different subjects
+% Arguments:
+% num_subjects - number of subjects out of 40 (1 - 40)
+% num_faces - number of faces (images) of each subject
+% Returns indices of required subset of images
 
 rand('seed', 1);
 subjects = randperm(40, num_subjects);
-X = randperm(10, 1);
-images_sub = zeros(size(images, 1), num_subjects);
-
-for i=1:num_subjects
-    images_sub(:, i) = images(:, subjects(i) * 10 + X);
+X = randperm(10, num_faces);
+% images_sub = zeros(size(images, 1), num_subjects);
+indices = [];
+for x=X
+    indices = [indices, subjects * 10 + x];
 end
-
